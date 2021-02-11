e15632f552d063923f78af7407e2fae2e3a40dd0
Satvik2001Naik <56932274+Satvik2001Naik@users.noreply.github.com>
2021-02-11 11:58:23 +0530

    Add files via upload

---
+++ student_module/enquiry_form_style.php
@@ -0,0 +1,41 @@
+<?php
+header('Content-type:text/css;');
+?>
+<style>
+*{
+    box-sizing: border-box;
+}
+.main-block {
+    /*border: 2px solid red;*/
+    border-radius: 5px;
+    background-color: #d6cfc7;
+    padding: 20px;
+    height: 65%;
+    width: 40%;
+    margin: auto;
+}
+label {
+    padding: 12px 12px 12px 0;
+    display: inline-block;
+    text-decoration: solid;
+}
+.f1 {
+    width: 100%;
+    padding: 12px;
+    border: 1px solid black;
+    border-radius: 4px;
+    resize: vertical;
+}
+input[type=submit] {
+    background-color: white;
+    color: black;
+    padding: 12px 20px;
+    border: 1px solid black;
+    border-radius: 4px;
+    cursor: pointer;
+    float: none;
+}
+input[typr=submit] :hover {
+    background-color: aqua;
+}
+</style>
